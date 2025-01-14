7337676547e98eeeb0736e85465becf680b58998
SamanthaBurn <SamanthaBurn@users.noreply.github.com>
2025-01-14 16:56:31 +0000

    Create extract_data.R
    Added an example of R code in the code directory

---
+++ code/extract_data.R
@@ -0,0 +1,18 @@
+## MSc Introduction to Health Analytics Group Project Code
+
+## Install necessary packages
+## This is a list of the packages you might need. Just add to the list if you want to install more.
+packages = (c("ipumsr", "dplyr", "ggplot2", "tidyr", "here", "rmdformats", "srvyr"))
+
+## Now load or install&load all
+package.check <- lapply(
+  packages,
+  FUN = function(x) {
+    if (!require(x, character.only = TRUE)) {
+      install.packages(x, dependencies = TRUE)
+      library(x, character.only = TRUE)
+    }
+  }
+)
+
+## Set the working directory
